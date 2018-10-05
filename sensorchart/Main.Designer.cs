061b141d50b9d54f502e7c8a71b814e82f24ae81
GhillieSuit <37437658+GhillieSuit@users.noreply.github.com>
2018-10-05 20:36:11 +0900

    Completing the function implementation

--- sensorchart/Main.Designer.cs
+++ sensorchart/Main.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.dtp_from_date = new System.Windows.Forms.DateTimePicker();
             this.lbl_from = new System.Windows.Forms.Label();
             this.dtp_to_date = new System.Windows.Forms.DateTimePicker();
@@ -40,6 +40,21 @@
             this.dtp_to_time = new System.Windows.Forms.DateTimePicker();
             this.lbx_list = new System.Windows.Forms.ListBox();
             this.btn_list = new System.Windows.Forms.Button();
+            this.btn_now = new System.Windows.Forms.Button();
+            this.btn_to_m1d = new System.Windows.Forms.Button();
+            this.btn_to_m1h = new System.Windows.Forms.Button();
+            this.btn_to_m10m = new System.Windows.Forms.Button();
+            this.btn_to_p10m = new System.Windows.Forms.Button();
+            this.btn_to_p1h = new System.Windows.Forms.Button();
+            this.btn_to_p1d = new System.Windows.Forms.Button();
+            this.btn_from_m1d = new System.Windows.Forms.Button();
+            this.btn_from_p1d = new System.Windows.Forms.Button();
+            this.btn_from_m1h = new System.Windows.Forms.Button();
+            this.btn_from_p1h = new System.Windows.Forms.Button();
+            this.btn_from_m10m = new System.Windows.Forms.Button();
+            this.btn_from_p10m = new System.Windows.Forms.Button();
+            this.ckb_auto = new System.Windows.Forms.CheckBox();
+            this.btn_apply = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,6 +66,7 @@
             this.dtp_from_date.Name = "dtp_from_date";
             this.dtp_from_date.Size = new System.Drawing.Size(170, 21);
             this.dtp_from_date.TabIndex = 2;
+            this.dtp_from_date.Value = new System.DateTime(2018, 10, 1, 0, 0, 0, 0);
             // 
             // lbl_from
             // 
@@ -83,21 +99,23 @@
             // 
             // chart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart.Legends.Add(legend2);
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
             this.chart.Location = new System.Drawing.Point(15, 122);
             this.chart.Name = "chart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart.Series.Add(series2);
             this.chart.Size = new System.Drawing.Size(1237, 547);
             this.chart.TabIndex = 10;
             this.chart.TabStop = false;
             this.chart.Text = "Chart";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "Chart";
+            this.chart.Titles.Add(title1);
             // 
             // dtp_from_time
             // 
@@ -108,7 +126,7 @@
             this.dtp_from_time.ShowUpDown = true;
             this.dtp_from_time.Size = new System.Drawing.Size(90, 21);
             this.dtp_from_time.TabIndex = 3;
-            this.dtp_from_time.Value = new System.DateTime(2018, 10, 3, 8, 9, 0, 0);
+            this.dtp_from_time.Value = new System.DateTime(2018, 10, 1, 0, 0, 0, 0);
             // 
             // dtp_to_time
             // 
@@ -143,11 +161,176 @@
             this.btn_list.UseVisualStyleBackColor = true;
             this.btn_list.Click += new System.EventHandler(this.btn_list_Click);
             // 
+            // btn_now
+            // 
+            this.btn_now.Location = new System.Drawing.Point(372, 68);
+            this.btn_now.Name = "btn_now";
+            this.btn_now.Size = new System.Drawing.Size(45, 21);
+            this.btn_now.TabIndex = 11;
+            this.btn_now.Text = "now";
+            this.btn_now.UseVisualStyleBackColor = true;
+            this.btn_now.Click += new System.EventHandler(this.btn_now_Click);
+            // 
+            // btn_to_m1d
+            // 
+            this.btn_to_m1d.Location = new System.Drawing.Point(525, 68);
+            this.btn_to_m1d.Name = "btn_to_m1d";
+            this.btn_to_m1d.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_m1d.TabIndex = 11;
+            this.btn_to_m1d.Text = "-1d";
+            this.btn_to_m1d.UseVisualStyleBackColor = true;
+            this.btn_to_m1d.Click += new System.EventHandler(this.btn_to_m1d_Click);
+            // 
+            // btn_to_m1h
+            // 
+            this.btn_to_m1h.Location = new System.Drawing.Point(474, 68);
+            this.btn_to_m1h.Name = "btn_to_m1h";
+            this.btn_to_m1h.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_m1h.TabIndex = 11;
+            this.btn_to_m1h.Text = "-1h";
+            this.btn_to_m1h.UseVisualStyleBackColor = true;
+            this.btn_to_m1h.Click += new System.EventHandler(this.btn_to_m1h_Click);
+            // 
+            // btn_to_m10m
+            // 
+            this.btn_to_m10m.Location = new System.Drawing.Point(423, 68);
+            this.btn_to_m10m.Name = "btn_to_m10m";
+            this.btn_to_m10m.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_m10m.TabIndex = 11;
+            this.btn_to_m10m.Text = "-10m";
+            this.btn_to_m10m.UseVisualStyleBackColor = true;
+            this.btn_to_m10m.Click += new System.EventHandler(this.btn_to_m10m_Click);
+            // 
+            // btn_to_p10m
+            // 
+            this.btn_to_p10m.Location = new System.Drawing.Point(423, 93);
+            this.btn_to_p10m.Name = "btn_to_p10m";
+            this.btn_to_p10m.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_p10m.TabIndex = 11;
+            this.btn_to_p10m.Text = "+10m";
+            this.btn_to_p10m.UseVisualStyleBackColor = true;
+            this.btn_to_p10m.Click += new System.EventHandler(this.btn_to_p10m_Click);
+            // 
+            // btn_to_p1h
+            // 
+            this.btn_to_p1h.Location = new System.Drawing.Point(474, 92);
+            this.btn_to_p1h.Name = "btn_to_p1h";
+            this.btn_to_p1h.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_p1h.TabIndex = 11;
+            this.btn_to_p1h.Text = "+1h";
+            this.btn_to_p1h.UseVisualStyleBackColor = true;
+            this.btn_to_p1h.Click += new System.EventHandler(this.btn_to_p1h_Click);
+            // 
+            // btn_to_p1d
+            // 
+            this.btn_to_p1d.Location = new System.Drawing.Point(525, 92);
+            this.btn_to_p1d.Name = "btn_to_p1d";
+            this.btn_to_p1d.Size = new System.Drawing.Size(45, 21);
+            this.btn_to_p1d.TabIndex = 11;
+            this.btn_to_p1d.Text = "+1d";
+            this.btn_to_p1d.UseVisualStyleBackColor = true;
+            this.btn_to_p1d.Click += new System.EventHandler(this.btn_to_p1d_Click);
+            // 
+            // btn_from_m1d
+            // 
+            this.btn_from_m1d.Location = new System.Drawing.Point(525, 7);
+            this.btn_from_m1d.Name = "btn_from_m1d";
+            this.btn_from_m1d.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_m1d.TabIndex = 11;
+            this.btn_from_m1d.Text = "-1d";
+            this.btn_from_m1d.UseVisualStyleBackColor = true;
+            this.btn_from_m1d.Click += new System.EventHandler(this.btn_from_m1d_Click);
+            // 
+            // btn_from_p1d
+            // 
+            this.btn_from_p1d.Location = new System.Drawing.Point(525, 31);
+            this.btn_from_p1d.Name = "btn_from_p1d";
+            this.btn_from_p1d.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_p1d.TabIndex = 11;
+            this.btn_from_p1d.Text = "+1d";
+            this.btn_from_p1d.UseVisualStyleBackColor = true;
+            this.btn_from_p1d.Click += new System.EventHandler(this.btn_from_p1d_Click);
+            // 
+            // btn_from_m1h
+            // 
+            this.btn_from_m1h.Location = new System.Drawing.Point(474, 7);
+            this.btn_from_m1h.Name = "btn_from_m1h";
+            this.btn_from_m1h.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_m1h.TabIndex = 11;
+            this.btn_from_m1h.Text = "-1h";
+            this.btn_from_m1h.UseVisualStyleBackColor = true;
+            this.btn_from_m1h.Click += new System.EventHandler(this.btn_from_m1h_Click);
+            // 
+            // btn_from_p1h
+            // 
+            this.btn_from_p1h.Location = new System.Drawing.Point(474, 31);
+            this.btn_from_p1h.Name = "btn_from_p1h";
+            this.btn_from_p1h.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_p1h.TabIndex = 11;
+            this.btn_from_p1h.Text = "+1h";
+            this.btn_from_p1h.UseVisualStyleBackColor = true;
+            this.btn_from_p1h.Click += new System.EventHandler(this.btn_from_p1h_Click);
+            // 
+            // btn_from_m10m
+            // 
+            this.btn_from_m10m.Location = new System.Drawing.Point(423, 7);
+            this.btn_from_m10m.Name = "btn_from_m10m";
+            this.btn_from_m10m.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_m10m.TabIndex = 11;
+            this.btn_from_m10m.Text = "-10m";
+            this.btn_from_m10m.UseVisualStyleBackColor = true;
+            this.btn_from_m10m.Click += new System.EventHandler(this.btn_from_m10m_Click);
+            // 
+            // btn_from_p10m
+            // 
+            this.btn_from_p10m.Location = new System.Drawing.Point(423, 32);
+            this.btn_from_p10m.Name = "btn_from_p10m";
+            this.btn_from_p10m.Size = new System.Drawing.Size(45, 21);
+            this.btn_from_p10m.TabIndex = 11;
+            this.btn_from_p10m.Text = "+10m";
+            this.btn_from_p10m.UseVisualStyleBackColor = true;
+            this.btn_from_p10m.Click += new System.EventHandler(this.btn_from_p10m_Click);
+            // 
+            // ckb_auto
+            // 
+            this.ckb_auto.AutoSize = true;
+            this.ckb_auto.Location = new System.Drawing.Point(576, 71);
+            this.ckb_auto.Name = "ckb_auto";
+            this.ckb_auto.Size = new System.Drawing.Size(96, 16);
+            this.ckb_auto.TabIndex = 12;
+            this.ckb_auto.Text = "Auto Refresh";
+            this.ckb_auto.UseVisualStyleBackColor = true;
+            // 
+            // btn_apply
+            // 
+            this.btn_apply.Location = new System.Drawing.Point(576, 90);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(96, 23);
+            this.btn_apply.TabIndex = 13;
+            this.btn_apply.Text = "Apply";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            this.btn_apply.Click += new System.EventHandler(this.btn_apply_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.btn_apply);
+            this.Controls.Add(this.ckb_auto);
+            this.Controls.Add(this.btn_from_p10m);
+            this.Controls.Add(this.btn_to_p10m);
+            this.Controls.Add(this.btn_from_m10m);
+            this.Controls.Add(this.btn_from_p1h);
+            this.Controls.Add(this.btn_to_m10m);
+            this.Controls.Add(this.btn_from_m1h);
+            this.Controls.Add(this.btn_to_p1h);
+            this.Controls.Add(this.btn_from_p1d);
+            this.Controls.Add(this.btn_to_m1h);
+            this.Controls.Add(this.btn_from_m1d);
+            this.Controls.Add(this.btn_to_p1d);
+            this.Controls.Add(this.btn_to_m1d);
+            this.Controls.Add(this.btn_now);
             this.Controls.Add(this.btn_list);
             this.Controls.Add(this.lbx_list);
             this.Controls.Add(this.dtp_to_time);
@@ -179,6 +362,21 @@
         private System.Windows.Forms.DateTimePicker dtp_to_time;
         private System.Windows.Forms.Button btn_list;
         public System.Windows.Forms.ListBox lbx_list;
+        private System.Windows.Forms.Button btn_now;
+        private System.Windows.Forms.Button btn_to_m1d;
+        private System.Windows.Forms.Button btn_to_m1h;
+        private System.Windows.Forms.Button btn_to_m10m;
+        private System.Windows.Forms.Button btn_to_p10m;
+        private System.Windows.Forms.Button btn_to_p1h;
+        private System.Windows.Forms.Button btn_to_p1d;
+        private System.Windows.Forms.Button btn_from_m1d;
+        private System.Windows.Forms.Button btn_from_p1d;
+        private System.Windows.Forms.Button btn_from_m1h;
+        private System.Windows.Forms.Button btn_from_p1h;
+        private System.Windows.Forms.Button btn_from_m10m;
+        private System.Windows.Forms.Button btn_from_p10m;
+        private System.Windows.Forms.CheckBox ckb_auto;
+        private System.Windows.Forms.Button btn_apply;
     }
 }
 
--- sensorchart/Main.cs
+++ sensorchart/Main.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using System.Windows.Forms.DataVisualization.Charting;
 using MySql.Data.MySqlClient;
 
 namespace sensorchart
@@ -28,7 +29,14 @@ namespace sensorchart
 
         private void Main_Load(object sender, EventArgs e)
         {
-            List<string[]> lists = new List<string[]>();
+            dtp_to_date.Value = DateTime.Now;
+            dtp_to_time.Value = DateTime.Now;
+            dtp_from_date.Value = DateTime.Now.AddHours(-1);
+            dtp_from_time.Value = DateTime.Now.AddHours(-1);
+
+
+            /* 아래 내용은 차트 테스트 입니다. */
+            /*List<string[]> lists = new List<string[]>();
 
             var MConn = new MySqlConnection(strSql);
             MConn.Open();
@@ -53,13 +61,160 @@ namespace sensorchart
             {
                 chart.Series[0].Points.AddXY(value[3], value[2]);
             }
-            MConn.Close();
+            MConn.Close();*/
+            /* 위의 내용은 차트 테스트 입니다. */
+
         }
 
         private void btn_list_Click(object sender, EventArgs e)
         {
             Sheet sheetform = new Sheet(this);
             sheetform.Show();
+        }
+
+        /* 데이트 타임 픽커의 밸류값을 조정하는 버튼입니다. */
+        private void btn_from_m10m_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddMinutes(-10);
+            dtp_from_time.Value = dtp_from_time.Value.AddMinutes(-10);
+        }
+
+        private void btn_from_p10m_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddMinutes(10);
+            dtp_from_time.Value = dtp_from_time.Value.AddMinutes(10);
+        }
+
+        private void btn_from_m1h_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddHours(-1);
+            dtp_from_time.Value = dtp_from_time.Value.AddHours(-1);
+        }
+
+        private void btn_from_p1h_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddHours(1);
+            dtp_from_time.Value = dtp_from_time.Value.AddHours(1);
+        }
+
+        private void btn_from_m1d_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddDays(-1);
+            dtp_from_time.Value = dtp_from_time.Value.AddDays(-1);
+        }
+
+        private void btn_from_p1d_Click(object sender, EventArgs e)
+        {
+            dtp_from_date.Value = dtp_from_date.Value.AddDays(1);
+            dtp_from_time.Value = dtp_from_time.Value.AddDays(1);
+        }
+
+        private void btn_now_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = DateTime.Now;
+            dtp_to_time.Value = DateTime.Now;
+        }
+
+        private void btn_to_m10m_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddMinutes(-10);
+            dtp_to_time.Value = dtp_to_time.Value.AddMinutes(-10);
+        }
+
+        private void btn_to_p10m_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddMinutes(10);
+            dtp_to_time.Value = dtp_to_time.Value.AddMinutes(10);
+        }
+
+        private void btn_to_m1h_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddHours(-1);
+            dtp_to_time.Value = dtp_to_time.Value.AddHours(-1);
+        }
+
+        private void btn_to_p1h_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddHours(1);
+            dtp_to_time.Value = dtp_to_time.Value.AddHours(1);
+        }
+
+        private void btn_to_m1d_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddDays(-1);
+            dtp_to_time.Value = dtp_to_time.Value.AddDays(-1);
+        }
+
+        private void btn_to_p1d_Click(object sender, EventArgs e)
+        {
+            dtp_to_date.Value = dtp_to_date.Value.AddDays(1);
+            dtp_to_time.Value = dtp_to_time.Value.AddDays(1);
+        }
+        /* *************************************************** */
+
+        private void btn_apply_Click(object sender, EventArgs e)
+        {
+            var MConn = new MySqlConnection(strSql);
+            MConn.Open();
+            DataSet ds = new DataSet();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            List<string[]> lists = new List<string[]>();
+            List<string> split = new List<string>();
+            string result = "";
+            string date_from = "";
+            string date_to = "";
+
+            for (int i = 0; i < lbx_list.Items.Count; i++)
+            {
+                split.Add(lbx_list.Items[i].ToString());
+                result += split[i].Split('\n')[0];
+                if (i < lbx_list.Items.Count - 1)
+                    result += ",";
+            }
+
+            date_from += dtp_from_date.Value.ToString("yyyyMMdd");
+            date_from += dtp_from_time.Value.ToString("HHmm00");
+            date_to += dtp_to_date.Value.ToString("yyyyMMdd");
+            date_to += dtp_to_time.Value.ToString("HHmm00");
+
+            try {
+                string sql = "select seq, id, value, datetime from datasheet where id in(" + result + ") and datetime between date(" + date_from + ") and date(" + date_to + ") order by seq";   
+                adapter.SelectCommand = new MySqlCommand(sql, MConn);
+                adapter.Fill(ds);
+            } catch (MySql.Data.MySqlClient.MySqlException sqlerror) {
+
+            }
+
+            chart.Series.Clear();
+
+            for (int i = 0; i < lbx_list.Items.Count; i++)
+            {
+                chart.Series.Add("Series" + i);
+                chart.Series["Series" + i].LegendText = split[i];
+                chart.Series["Series" + i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+
+                foreach (DataRow r in ds.Tables[0].Rows)        //차트 데이터 입력
+                {
+                    if (r["id"].ToString()+'\n' == split[i].ToString())
+                    {
+                        string[] temp = new string[4];
+                        temp[0] = r["seq"].ToString();
+                        temp[1] = r["id"].ToString();
+                        temp[2] = r["value"].ToString();
+                        temp[3] = r["datetime"].ToString();
+                        lists.Add(temp);
+                    }
+                }
+
+                foreach (string[] value in lists)       //차트 그리기
+                {
+                    chart.Series["Series" + i].Points.AddXY(value[3], value[2]);
+                }
+                lists.Clear();
+            }
+            MConn.Close();
         }
     }
 }
